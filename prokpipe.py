5d639370a53c9a4adab94e4595d87489ee06870c
Aglab_lap3 <zilov@scamt-itmo.ru>
2020-07-22 14:34:17 +0300

    Pannopi alfa 0.0.1 version

--- prokpipe.py
+++ prokpipe.py
@@ -16,14 +16,24 @@ def main():
 
     import os
     import os.path
+    
+    if test:
+        workdir = "test_data/"
+    else:
+        workdir = "data/"
+        
+    if debug:
+        snake_debug = "-n"
+    else:
+        snake_debug = ""
 
     workdir_list = []
-    for i in os.listdir("data/"):
+    for i in os.listdir(workdir):
         if i.startswith("."):
             continue
         else:
-            if os.path.isdir("data/" + i) == True:
-                workdir_list.append(os.path.abspath("data/" + i))
+            if os.path.isdir(workdir + i) == True:
+                workdir_list.append(os.path.abspath(workdir + i))
 
     for workdir in workdir_list:
         #Reads prepare
@@ -34,21 +44,25 @@ def main():
         print(command)
         os.system(command)
         #Snakemake
-        command = "snakemake --cores %s --use-conda -n" % (threads)
+        command = "snakemake --cores %s --use-conda %s" % (cpus, snake_debug)
         print(command)
         os.system(command)
         
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Program description.')
-    parser.add_argument('-a','--assembler', help='assebler tool: spades, unicycler or skesa', required=True)
-    parser.add_argument('-n','--annotator', help='annotation tool: pgap or prokka', required=True)
-    parser.add_argument('-t','--threads', help='number of threads', required=True)
+    parser = argparse.ArgumentParser(description='Pannopi - a tool for prokariotic genome assembly and annotation.')
+    parser.add_argument('-a','--assembler', help='assebler tool: spades, unicycler or skesa [default = spades]', default="spades")
+    parser.add_argument('-n','--annotator', help='annotation tool: pgap or prokka [default = prokka]', default="prokka")
+    parser.add_argument('-c','--cpus', help='number of threads [default = 8]', default="8")
+    parser.add_argument('-t','--test', help='run pipeline on test data', action='store_true')
+    parser.add_argument('-d','--debug', help='debug mode', action='store_true')
     args = vars(parser.parse_args())
 
     assembler = args["assembler"]
     annotator = args["annotator"]
-    threads = args["threads"]
+    cpus = args["cpus"]
+    test = args["test"]
+    debug = args["debug"]
 
     main()
