22c4d7b846c364927de0a394bda72e6636de007f
zilov <zilov@scamt-itmo.ru>
2021-05-15 15:03:46 +0000

    update config_maker for new modes

--- config-maker.py
+++ config-maker.py
@@ -1,122 +1,127 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#@created: <data>
-#@author: <name>
-#@contact: <email>
+#@created: 15.05.2021
+#@author: Zilov Danil, GitHUB: zilov
+#@contact: zilov.d@gamail.com
 
-import sys
 import argparse
+import os
+import os.path
 
 
-def main():
+def main(forward_read, reverse_read, long_read, prefix, outdir, mode):
     ''' Function description.
     '''
     pass
+    execution_folder = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
+    config_file = f"{execution_folder}/config/config.yaml"
+    
+    config_parameters = f"""
+# MAIN PARAMETERS
 
-    import os
-    import os.path
-
-    config_file = "config/config.yaml"
-
-    for i in os.listdir(work_dir):
-        if i.endswith(".fastq"):
-            prefix = "_".join(i.split("_")[:-1])
-    path_to_reads = os.path.abspath(work_dir)
-    for i in os.listdir(work_dir):
-        if i.endswith("_1.fastq"):
-            raw_fastq_1 = path_to_reads + "/" + i
-            print(raw_fastq_1)
-        elif i.endswith("_2.fastq"):
-            raw_fastq_2 = path_to_reads + "/" + i
-            print(raw_fastq_2)
-
-    if assembler == "spades":
-        assembly_file = "scaffolds"
-        assembly_gfa = "assembly_graph_with_scaffolds.gfa"
-    elif assembler == "unicycler":
-        assembly_file = "assembly"
-    elif assembler == "skesa":
-        assembly_file = "??????????????" ##### УЗНАТЬ НАПИСАТЬ ДОПОЛНИТЬ
-
-    config = """
-assembler: "rules/{assembler}.sf"
-annotator: "rules/{annotator}.sf"
-raw_fastq_1: "{raw_fastq_1}"
-raw_fastq_2: "{raw_fastq_2}"
-path_to_reads: "{path_to_reads}"
-prefix: "{prefix}"
-
-#PREPARE
-trim_in_prefix: "{path_to_reads}/{prefix}"
-trim_out_prefix: "{path_to_reads}/preparation/trim/{prefix}"
-rmdub_in_prefix: "{path_to_reads}/preparation/trim/{prefix}.trim"
-rmdub_out_prefix: "{path_to_reads}/preparation/rmdub/{prefix}.rm"
-trim_out_file_1:  "{path_to_reads}/preparation/trim/{prefix}.trim_1.fastq"
-trim_out_file_2: "{path_to_reads}/preparation/trim/{prefix}.trim_2.fastq"
-rmdub_file_1: "{path_to_reads}/preparation/rmdub/{prefix}.rm_1.fastq"
-rmdub_file_2: "{path_to_reads}/preparation/rmdub/{prefix}.rm_2.fastq"
-
-#FASTQC
-fastqc1_dir: "{path_to_reads}/preparation/QC/fastqc/fastqc_raw"
-fastqc2_dir: "{path_to_reads}/preparation/QC/fastqc/fastqc_clean"
-fastqc_file1: "{path_to_reads}/preparation/QC/fastqc/fastqc_raw/{prefix}_1_fastqc.zip"
-fastqc_file2: "{path_to_reads}/preparation/QC/fastqc/fastqc_clean/{prefix}.rm_2_fastqc.zip"
-
-#JELLY FILES
-jellycount_file: "{path_to_reads}/preparation/QC/jellyfish/{prefix}.jf2"
-jellyhisto_file: "{path_to_reads}/preparation/QC/jellyfish/{prefix}.histo"
-scope_dir: "{path_to_reads}/preparation/QC/jellyfish/scope/"
-scope_file: "{path_to_reads}/preparation/QC/jellyfish/scope/plot.png"
+# databases
+eggnog_db: "/mnt/projects/databases/eggnog_db/"
+blast_nt_db: ""
+    """
+    
+    config_universal = f"""
+    
+# ASSEMBLY
 
 #ASSEMBLY
-assembly: "{path_to_reads}/genome_assembly/{assembler}/{assembly_file}.fasta"
-assembly_dir: "{path_to_reads}/genome_assembly/{assembler}"
-assembly_gfa: "{path_to_reads}/genome_assembly/{assembler}/{assembly_gfa}"
-
-#ASSEMBLY QUALITY CONTROL
-quast_dir: "{path_to_reads}/genome_assembly/QC/quast"
-quast_out_file: "{path_to_reads}/genome_assembly/QC/quast/report.txt"
-reference_dir: "{path_to_reads}/genome_assembly/QC/contera/reference/"
-reference_file: "{path_to_reads}/reference/{prefix}.fna"
-
-#BLAST
-blastn_out: "{path_to_reads}/genome_assembly/QC/blast/{prefix}.outfmt6"
-blast_db: "nt"
+assembly_dir: "{outdir}/genome_assembly/unicycler"
+assembly: "{outdir}/genome_assembly/unicycler/assembly.fasta"
 
 # FILTERING
 adapters_fasta: "tools/contera/adapters_db/adaptor_fasta.fna"
-assembly_filtered: "{path_to_reads}/genome_assembly/QC/contera/scaffolds_filtered.fasta"
-contera_dir:  "{path_to_reads}/genome_assembly/QC/contera/"
-contera_report: "{path_to_reads}/genome_assembly/QC/contera/{prefix}.contera_report.txt"
-adapters_report: "{path_to_reads}/genome_assembly/QC/contera/{prefix}.adapters_report.outfmt6"
+assembly_filtered: "{outdir}/genome_assembly/QC/contera/scaffolds_filtered.fasta"
+contera_dir:  "{outdir}/genome_assembly/QC/contera/"
+contera_report: "{outdir}/genome_assembly/QC/contera/{prefix}.contera_report.txt"
+adapters_report: "{outdir}/genome_assembly/QC/contera/{prefix}.adapters_report.outfmt6"
+
+#ASSEMBLY QUALITY CONTROL
+quast_dir: "{outdir}/genome_assembly/QC/quast"
+quast_out_file: "{outdir}/genome_assembly/QC/quast/report.txt"
 
 # STRUCTURAL ANNOTATION
-annotation_dir: "{path_to_reads}/struct_annotation/{annotator}/"
-annotation_faa: "{path_to_reads}/struct_annotation/{annotator}/{prefix}.faa"
-annotation_gbk: "{path_to_reads}/struct_annotation/{annotator}/{prefix}.gbk"
+# prokka
+annotation_dir: "{outdir}/struct_annotation/{annotator}/"
+annotation_faa: "{outdir}/struct_annotation/{annotator}/{prefix}.faa"
+annotation_gbk: "{outdir}/struct_annotation/{annotator}/{prefix}.gbk"
+
 
 # FUNCTIONAL ANNOTATION
-eggnog_dir: "{path_to_reads}/func_annotation/eggnog"
-eggnog_ann: "{path_to_reads}/func_annotation/eggnog/{prefix}.emapper.annotations"
-goann_outdir: "{path_to_reads}/func_annotation/goann/"
-goann_out: "{path_to_reads}/func_annotation/goann/{prefix}_genes_analysis.txt"
-funcan_dir: "{path_to_reads}/func_annotation/"
-megares: "{path_to_reads}/func_annotation/megares_report.txt"
-ncbi: "{path_to_reads}/func_annotation/ncbi_report.txt"
-virulence: "{path_to_reads}/func_annotation/virulence_report.txt"
-plasmids: "{path_to_reads}/func_annotation/plasmids_report.txt"
-serotype: "{path_to_reads}/func_annotation/serotype_ecoli.txt"
-funcan_sum: "{path_to_reads}/func_annotation/AMR_and_virulence_report.txt"
-mlst: "{path_to_reads}/func_annotation/mlst_type.txt"
+
+#eggnog
+eggnog_dir: "{outdir}/eggnog/"
+eggnog_prefix: "{prefix}"
+eggnog_out_annotation: "{outdir}/eggnog/{prefix}.emapper.annotations"
+eggnog_out_orthologs: "{outdir}/eggnog/{prefix}.emapper.seed_orthologs"
+
+funcan_dir: "{outdir}/func_annotation/"
+megares: "{outdir}/func_annotation/{prefix}_megares_report.txt"
+ncbi: "{outdir}/func_annotation/{prefix}_ncbi_report.txt"
+virulence: "{outdir}/func_annotation/{prefix}_virulence_report.txt"
+plasmids: "{outdir}/func_annotation/{prefix}_plasmids_report.txt"
+serotype: "{outdir}/func_annotation/{prefix}_serotype_ecoli.txt"
+funcan_sum: "{outdir}/func_annotation/{prefix}_AMR_and_virulence_report.txt"
+mlst: "{outdir}/func_annotation/{prefix}_mlst_type.txt"
 
 #RESULT STEP
-results_path: "{path_to_reads}/results/"
-results_file: "{path_to_reads}/results/{prefix}_genes_analysis.txt"
-    """.format(path_to_reads = path_to_reads, prefix = prefix,\
-    assembly_file = assembly_file, assembly_gfa = assembly_gfa, assembler = assembler, annotator = annotator, raw_fastq_1 = raw_fastq_1,\
-    raw_fastq_2 = raw_fastq_2)
+results_path: "{outdir}/results/"
+    """
+    
+    if forward_read:
+        config_short = f"""
 
+    # PREPARATION 
+
+    #v2trim
+    v2trim_dir: "{outdir}/preparation/v2trim/"
+    v2trim_in_file1: "{forward_read}"
+    v2trim_in_file2: "{reverse_read}"
+    v2trim_prefix: "{prefix}"
+    v2trim_out_file1: "{outdir}/preparation/v2trim/{prefix}.trim_1.fastq"
+    v2trim_out_file2: "{outdir}/preparation/v2trim/{prefix}.trim_2.fastq"
+    v2trim_out_statistics: "{outdir}/preparation/v2trim/{prefix}.stats"
+
+    #rmdup
+    rmdup_dir: "{outdir}/preparation/rmdup/"
+    rmdup_out_file1: "{outdir}/preparation/rmdup/{prefix}.rm_1.fastq"
+    rmdup_out_file2: "{outdir}/preparation/rmdup/{prefix}.rm_2.fastq"
+    rmdup_out_statistics: "{outdir}/preparation/rmdup/{prefix}.rm.stats"
+
+    #FASTQC
+    fastqc1_dir: "{outdir}/preparation/QC/fastqc/fastqc_raw"
+    fastqc2_dir: "{outdir}/preparation/QC/fastqc/fastqc_clean"
+    fastqc_file1: "{outdir}/preparation/QC/fastqc/fastqc_raw/{prefix}_1_fastqc.zip"
+    fastqc_file2: "{outdir}/preparation/QC/fastqc/fastqc_clean/{prefix}.rm_2_fastqc.zip"
+
+    #JELLY FILES
+    jellycount_file: "{outdir}/preparation/QC/jellyfish/{prefix}.jf2"
+    jellyhisto_file: "{outdir}/preparation/QC/jellyfish/{prefix}.histo"
+    scope_dir: "{outdir}/preparation/QC/jellyfish/scope/"
+    scope_file: "{outdir}/preparation/QC/jellyfish/scope/plot.png"
+    """
+        
+    if long_read:
+        config_long = f"""
+        long_reads_file = {long_read}
+        """
+            
+    
+    if mode == "short":
+        unicycler_rule = os.path.join(execution_folder,"rules/unicycler_short.smk")
+        config = config_paremeters + config_short + config_universal
+    elif mode == "long":
+        unicucler_rule = os.path.join(execution_folder,"rules/unicycler_long.smk")
+        config = config_parameters + config_long + config_universal
+    elif mode == "hybrid":
+        unicycler_rule = os.path.join(execution_folder,"rules/unicycler_hybrid.smk") 
+        config = config_parameters + config_short + config_long + config_universal
+    
+    
     with open(config_file, "w") as fw:
         fw.write(config)
 
@@ -124,13 +129,20 @@ results_file: "{path_to_reads}/results/{prefix}_genes_analysis.txt"
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Program description.')
-    parser.add_argument('-w', '--workdir', help='sample directory', required=True)
-    parser.add_argument('-a', '--assembler', help='assebler tool: spades unicycler or skesa', required=True)
-    parser.add_argument('-n', '--annotator', help='annotation tool: pgap or prokka', required=True)
+    parser.add_argument('-1', '--forward_read', help='sample directory', required=False, default=False)
+    parser.add_argument('-2', '--reverse_read', help='sample directory', required=False, default=False)
+    parser.add_argument('-l', '--long_read', help='sample directory', required=False, default=False)
+    parser.add_argument('-m', '--mode', help = 'Pannopi mode', required = True)
+    parser.add_argument('-p', '--prefix', help='Prefix for output files', required=True)
+    parser.add_argument('-o', '--outdir', help='Output directory', required=True)
     args = vars(parser.parse_args())
+    
+    # All of parameters goes in absolute paths from pannopi.py
+    forward_read = args["forward_read"]
+    reverse_read = args["reverse_read"]
+    long_read = args["long_read"]
+    outdir = args["outdir"]
+    mode = args["mode"]
+    prefix =args["prefix"]
 
-    work_dir = args["workdir"]
-    assembler = args["assembler"]
-    annotator = args["annotator"]
-
-    main()
+    main(forward_read, reverse_read, long_read, prefix, outdir, mode)
