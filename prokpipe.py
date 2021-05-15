03b26dac5792a7657056916f7fe9e889a2961fc4
zilov <zilov@scamt-itmo.ru>
2021-05-15 13:56:23 +0000

    add new arguments for modes in argparse

--- prokpipe.py
+++ prokpipe.py
@@ -5,8 +5,10 @@
 #@author: <name>
 #@contact: <email>
 
-import sys
 import argparse
+import os
+import os.path
+from inspect import getsourcefile
 
 
 def main():
@@ -14,8 +16,7 @@ def main():
     '''
     pass
 
-    import os
-    import os.path
+
     
     if test:
         workdir = "test_data/"
@@ -52,17 +53,59 @@ def main():
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Pannopi - a tool for prokariotic genome assembly and annotation.')
-    parser.add_argument('-a','--assembler', help='assebler tool: spades, unicycler or skesa [default = spades]', default="spades")
-    parser.add_argument('-n','--annotator', help='annotation tool: pgap or prokka [default = prokka]', default="prokka")
-    parser.add_argument('-c','--cpus', help='number of threads [default = 8]', default="8")
-    parser.add_argument('-t','--test', help='run pipeline on test data', action='store_true')
+    parser.add_argument('-m','--mode', help="mode to use [default = short]", 
+                        choices=["short", "long", "hybrid"], default="short")
+    parser.add_argument('-r','--reference', help="path to reference asssembly in FASTA format", required=False. default="0")
+    parser.add_argument('-1','--forward_reads', help="path to forward short read file in FASTQ format", default="0")
+    parser.add_argument('-2','--reverse_reads', help="path to reverse short read file in FASTQ format", default="0")
+    parser.add_argument('-l','--long_reads', help="path to long read file in FASTQ format", default="0")
+    parser.add_argument('-o','--outdir', help='output directory', required=True)
+    parser.add_argument('-t','--threads', help='number of threads [default == 8]', default = "8")
     parser.add_argument('-d','--debug', help='debug mode', action='store_true')
     args = vars(parser.parse_args())
 
-    assembler = args["assembler"]
-    annotator = args["annotator"]
-    cpus = args["cpus"]
-    test = args["test"]
+    reference_fasta = args["reference"]
+    threads = args["threads"]
     debug = args["debug"]
-
-    main()
+    mode = args["mode"]
+    forward_read = args["forward_reads"]
+    reverse_read = args["reverse_reads"]
+    long_read = args["long_reads"]
+    outdir = os.path.abspath(args["outdir"])
+    
+    execution_folder = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
+        
+        
+    # Check that all files required for each mode are in    
+    if mode == "short":
+        if forward_read == "0" or reverse_read == "0":
+            parser.error("\nShort-reads mode requires -1 {path_to_forward_read} and -2 {path_to_reverse_read}!")
+        elif long_read != "0":
+            parser.error("\nShort-reads mode requires -1 {path_to_forward_read} and -2 {path_to_reverse_read} only!\
+             To analyse long-reads use long-reads mode or hybrid mode")
+        else:
+            forward_read = os.path.abspath(forward_read)
+            reverse_read = os.path.abspath(reverse_read)
+            
+    elif mode == "long":
+        if long_read == "0":
+            parser.error("\nLong-read mode requires -l {path_to_long_reads.fq}!")
+        elif forward_read != "0" or reverse_read != "0":
+            parser.error("\nLong-reads mode requires -l {path_to_long_reads.fq} only!\ 
+            To analyse short-reads use short-reads mode or hybrid mode")
+        else:
+            long_read = os.path.abspath(long_read)
+            
+    elif mode == "fasta_rna_faa":
+        if forward_rna_read == "0" or reverse_rna_read == "0" or faa_file == "0":
+            parser.error("\nHybrid mode requires -1 {path_to_forward_read} AND -2 {path_to_reverse_read} AND -l {path_to_long_reads.fq}!")
+        else:
+            faa_file = os.path.abspath(faa_file)
+            forward_rna_read = os.path.abspath(forward_rna_read)
+            reverse_rna_read = os.path.abspath(reverse_rna_read)
+            
+    if reference_fasta != "0":
+        reference_fasta = os.path.abspath(reference_fasta)
+    else:
+        reference_fasta = False
+    main(reference_fasta, forward_read, reverse_read, long_read, outdir, threads, mode, execution_folder, debug)
