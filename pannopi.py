11226538d63a0574d7b319407c0a1da21a9a1264
zilov <zilov@scamt-itmo.ru>
2021-05-31 16:24:29 +0000

    add check for databases before run

--- pannopi.py
+++ pannopi.py
@@ -13,10 +13,56 @@ from datetime import datetime
 import string
 import random
 
+def check_for_eggnog_db(settings_file):
+    if not os.path.exists(settings_file):
+        raise ValueError("Pannopi requires eggnog-database, please download or set it "
+                         "with pannopi_download_db.py!")
+    else:
+        return True
+#         ask_for_database = input("Pannopi requires eggnog-database (~50GB), do you wanna download it? [y/n]: ")
+#         if ask_for_database == "y":
+#             ask_for_outdir = input("Where do you wanna install it? Provide path to folder to store database: ")
+#             if ask_for_outdir:
+#                 output = ask_for_outdir
+#             ask_for_blast_db = input("Do you want to download blastn database to filter your assembly (~100GB)? [y/n]: ")
+#             if ask_for_blast_db == "y":
+#                 run = f"pannopi_download_db.py -m all -o {output}"
+#             else:
+#                 run = f"pannopi_download_db.py -o {output}"
+#             os.system(run)
+#             return True
+#         else:
+#             ask_if_it_installed = input("Do you wanna provide path path to eggnog-database? [y/n]: ")
+#             if ask_if_it_installed == "y":
+#                 path_to_eggnog = ""
+#
+#     else:
+#         return True
+#
+# def check_for_blast_db(settings_file):
+#     if not os.path.exists(settings_file):
+#         ask_for_database = input("Pannopi needs blastn database to make assembly-content report (~110GB), do you wanna download it? [y/n]: ")
+#         if ask_for_database == "y":
+#             ask_for_outdir = input("Where do you wanna install it? Provide path to folder to store database: ")
+#             if ask_for_outdir:
+#                 output = ask_for_outdir
+#             if ask_for_blast_db == "y":
+#                 run = f"pannopi_download_db.py -m all -o {output}"
+#             else:
+#                 run = f"pannopi_download_db.py -o {output}"
+#             os.system(run)
+#             return True
+#         else:
+#             ask_if_it_installed = input("Do you have blast nt database installed on your system? [y/n]: ")
+#             if ask_if_it_installed == "y":
+#                 path_to_blast = input("Provide path path to blast nt database: ")
+#
+#             raise ValueError(
+#                 "Pannopi requires eggnog-database, please download or set it with pannopi_download_db.py!")
 
 def config_short(forward_read, reverse_read, reference_fasta, prefix, outdir, mode, execution_folder, config_file):
     #Config-maker run
-    command = f"{execution_folder}/config-maker.py -1 {forward_read} -2 {reverse_read} " \
+    command = f"{execution_folder}/scripts/config-maker.py -1 {forward_read} -2 {reverse_read} " \
               f"-r {reference_fasta} -p {prefix} -o {outdir} -m {mode} -c {config_file}"
     print(command)
     os.system(command)
@@ -24,7 +70,7 @@ def config_short(forward_read, reverse_read, reference_fasta, prefix, outdir, mo
 
 def config_long(long_read,reference_fasta, prefix, outdir, mode, execution_folder, config_file):
     #Config-maker run
-    command = f"{execution_folder}/config-maker.py -l {long_read} " \
+    command = f"{execution_folder}/scripts/config-maker.py -l {long_read} " \
               f"-r {reference_fasta} -p {prefix} -o {outdir} -m {mode} -c {config_file}"
     print(command)
     os.system(command)
@@ -32,7 +78,7 @@ def config_long(long_read,reference_fasta, prefix, outdir, mode, execution_folde
 
 def config_hybrid(forward_read, reverse_read, long_read,reference_fasta, prefix, outdir, mode, execution_folder, config_file):
     #Config-maker run
-    command = f"{execution_folder}/config-maker.py -1 {forward_read} -2 {reverse_read} " \
+    command = f"{execution_folder}/scripts/config-maker.py -1 {forward_read} -2 {reverse_read} " \
               f"-l {long_read} -r {reference_fasta} -p {prefix} -o {outdir} -m {mode} -c {config_file}"
     print(command)
     os.system(command)
@@ -81,6 +127,7 @@ if __name__ == '__main__':
     execution_time = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
     random_letters = "".join([random.choice(string.ascii_letters) for n in range(3)])
     config_file = os.path.join(execution_folder, f"config/config_{random_letters}{execution_time}.yaml")
+    settings_file = os.path.join(execution_folder, "workflows/settings.snakefile")
 
     if reference_fasta != "0":
         reference_fasta = os.path.abspath(reference_fasta)
@@ -92,37 +139,39 @@ if __name__ == '__main__':
             prefix = os.path.basename(forward_read).split("_")[0]
         elif long_read != "0":
             prefix = os.path.basename(long_read).split(".")[0]
-        
-    # Check that all files required for each mode are in and run pipeline
-    if mode == "short":
-        if forward_read == "0" or reverse_read == "0":
-            parser.error("\nShort-reads mode requires -1 {path_to_forward_read} and -2 {path_to_reverse_read}!")
-        elif long_read != "0":
-            parser.error("\nShort-reads mode requires -1 {path_to_forward_read} and -2 {path_to_reverse_read} only!\nTo analyse long-reads use long-reads mode or hybrid mode")
-        else:
-            forward_read = os.path.abspath(forward_read)
-            reverse_read = os.path.abspath(reverse_read)
-            snakefile = os.path.join(execution_folder, "workflows/short_mode.snakefile")
-            config_short(forward_read, reverse_read, reference_fasta, prefix, outdir, mode, execution_folder, config_file)
-            
-    elif mode == "long":
-        if long_read == "0":
-            parser.error("\nLong-read mode requires -l {path_to_long_reads.fq}!")
-        elif forward_read != "0" or reverse_read != "0":
-            parser.error("\nLong-reads mode requires -l {path_to_long_reads.fq} only!\nTo analyse short-reads use short-reads mode or hybrid mode")
-        else:
-            long_read = os.path.abspath(long_read)
-            snakefile = os.path.join(execution_folder, "workflows/long_mode.snakefile")
-            config_long(long_read, reference_fasta, prefix, outdir, mode, execution_folder, config_file)
-            
-    elif mode == "hybrid":
-        if forward_read == "0" or reverse_read == "0" or long_read == "0":
-            parser.error("\nHybrid mode requires -1 {path_to_forward_read} AND -2 {path_to_reverse_read} AND -l {path_to_long_reads.fq}!")
-        else:
-            long_read = os.path.abspath(long_read)
-            forward_read = os.path.abspath(forward_read)
-            reverse_read = os.path.abspath(reverse_read)
-            snakefile = os.path.join(execution_folder, "workflows/hybrid_mode.snakefile")
-            config_hybrid(forward_read, reverse_read, long_read,reference_fasta, prefix, outdir, mode, execution_folder, config_file)
 
-snakerun(snakefile, threads, config_file, debug)
+
+    if check_for_databases(settings_file):
+        # Check that all files required for each mode are in and run pipeline
+        if mode == "short":
+            if forward_read == "0" or reverse_read == "0":
+                parser.error("\nShort-reads mode requires -1 {path_to_forward_read} and -2 {path_to_reverse_read}!")
+            elif long_read != "0":
+                parser.error("\nShort-reads mode requires -1 {path_to_forward_read} and -2 {path_to_reverse_read} only!\nTo analyse long-reads use long-reads mode or hybrid mode")
+            else:
+                forward_read = os.path.abspath(forward_read)
+                reverse_read = os.path.abspath(reverse_read)
+                snakefile = os.path.join(execution_folder, "workflows/short_mode.snakefile")
+                config_short(forward_read, reverse_read, reference_fasta, prefix, outdir, mode, execution_folder, config_file)
+
+        elif mode == "long":
+            if long_read == "0":
+                parser.error("\nLong-read mode requires -l {path_to_long_reads.fq}!")
+            elif forward_read != "0" or reverse_read != "0":
+                parser.error("\nLong-reads mode requires -l {path_to_long_reads.fq} only!\nTo analyse short-reads use short-reads mode or hybrid mode")
+            else:
+                long_read = os.path.abspath(long_read)
+                snakefile = os.path.join(execution_folder, "workflows/long_mode.snakefile")
+                config_long(long_read, reference_fasta, prefix, outdir, mode, execution_folder, config_file)
+
+        elif mode == "hybrid":
+            if forward_read == "0" or reverse_read == "0" or long_read == "0":
+                parser.error("\nHybrid mode requires -1 {path_to_forward_read} AND -2 {path_to_reverse_read} AND -l {path_to_long_reads.fq}!")
+            else:
+                long_read = os.path.abspath(long_read)
+                forward_read = os.path.abspath(forward_read)
+                reverse_read = os.path.abspath(reverse_read)
+                snakefile = os.path.join(execution_folder, "workflows/hybrid_mode.snakefile")
+                config_hybrid(forward_read, reverse_read, long_read,reference_fasta, prefix, outdir, mode, execution_folder, config_file)
+
+    snakerun(snakefile, threads, config_file, debug)
